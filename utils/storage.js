2447acb228248d6ed36cf92d49268a42ea54283a
mmand213 <165223995+mmand213@users.noreply.github.com>
2025-05-07 20:46:18 -0400

    Delete utils directory

--- utils/storage.js
+++
@@ -1,6 +0,0 @@
-export function loadProjects() {
-  return JSON.parse(localStorage.getItem('projects') || '[]');
-}
-export function saveProjects(projects) {
-  localStorage.setItem('projects', JSON.stringify(projects));
-}
