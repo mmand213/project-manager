9712c53a5849c0d9e2f7b0dfe799e80652e5ebb6
mmand213 <165223995+mmand213@users.noreply.github.com>
2025-05-07 20:40:25 -0400

    Add files via upload

---
+++ utils/storage.js
@@ -0,0 +1,6 @@
+export function loadProjects() {
+  return JSON.parse(localStorage.getItem('projects') || '[]');
+}
+export function saveProjects(projects) {
+  localStorage.setItem('projects', JSON.stringify(projects));
+}
