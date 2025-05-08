927f1ef78b1c2e5d453d47c93d71f8527895533c
mmand213 <165223995+mmand213@users.noreply.github.com>
2025-05-08 00:00:06 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
+  content: ['./index.html','./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0055FF',       // deep blue
+        primaryLight: '#3f7fff',  // hover highlight
+        accent: '#00D1FF',        // aqua accent
+        pageBg: '#F4F7FA',        // subtle gray page background
+        cardBg: '#FFFFFF',        // white card
+      },
+      fontFamily: {
+        poppins: ['Poppins','sans-serif'],
+      }
+    }
+  },
+}
