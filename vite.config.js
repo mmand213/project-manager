aed1c9c1b9379ff2e6ec56326b7ce4c6c8987608
mmand213 <165223995+mmand213@users.noreply.github.com>
2025-05-10 04:15:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react              from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/project-manager/',
+  base: '/project-manager/',    // <-- your GH Pages sub-path
   plugins: [react()],
 })
