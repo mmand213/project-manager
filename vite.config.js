01e68ab0af8e06e68921d9da5fec34fbe51a85ca
mmand213 <165223995+mmand213@users.noreply.github.com>
2025-05-10 03:52:55 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/project-manager/',
+  base: '/project-manager/',   // <-- your GitHub Pages repo name
   plugins: [react()],
 })
