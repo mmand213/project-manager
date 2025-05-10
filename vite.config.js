2033e26ad4d887b7397a1b450713b3c3a53ac749
mmand213 <165223995+mmand213@users.noreply.github.com>
2025-05-10 03:04:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/it-infra-pm/',
+  base: '/project-manager/',
   plugins: [react()],
 })
