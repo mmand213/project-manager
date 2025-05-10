cdc277ceea23f39d7d819ffa4d504234d62f4752
mmand213 <165223995+mmand213@users.noreply.github.com>
2025-05-10 15:57:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react              from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/project-manager/',    // <-- your GH Pages sub-path
+  base: '/project-manager/',   // <-- your repo name here
   plugins: [react()],
-})
+});
